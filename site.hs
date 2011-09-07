5203506351638f8c72a824594c53085b14407acb
Fernand Pajot <fernandp@gmail.com>
2011-09-07 20:37:42 +0200

    initial commit

---
+++ site.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Prelude hiding (id)
+import Control.Category (id)
+import Control.Arrow ((>>>), (***), arr)
+import Data.Monoid (mempty, mconcat)
+
+import Hakyll
+
+main :: IO ()
+main = hakyll $ do
+    -- Compress CSS
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    -- Render posts
+    match "posts/*" $ do
+        route   $ setExtension ".html"
+        compile $ pageCompiler
+            >>> applyTemplateCompiler "templates/post.html"
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
+
+    -- Index
+    match  "index.html" $ route idRoute
+    create "index.html" $ constA mempty
+        >>> requireAllA "posts/*" addPostList
+        >>> applyTemplateCompiler "templates/index.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+    -- Render RSS feed
+    match  "rss.xml" $ route idRoute
+    create "rss.xml" $
+        requireAll_ "posts/*" >>> renderRss feedConfiguration
+
+    -- Read templates
+    match "templates/*" $ compile templateCompiler
+
+-- | Auxiliary compiler: generate a post list from a list of given posts, and
+-- add it to the current page under @$posts@
+--
+prettyDate :: Page a -> Page a
+prettyDate = renderDateField "pdate" "%d %b %Y" "unknown"
+
+addPostList :: Compiler (Page String, [Page String]) (Page String)
+addPostList = setFieldA "posts" $
+        arr (map prettyDate)
+        >>> arr (reverse . chronological)
+        >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
+        >>> arr mconcat
+        >>> arr pageBody
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
+    { feedTitle       = "Fernand Pajot's blog feed"
+    , feedDescription = "thoughts about *"
+    , feedAuthorName  = "Fernand Pajot"
+    , feedRoot        = "http://epigram.me"
+    }
